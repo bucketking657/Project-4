9fcd843885d958022023f1315f9f92a87284f2ed
zionc <zchilagan678@gmail.com>
2019-04-05 17:01:02 -0400

    Add files via upload

--- Bracket.java
+++ Bracket.java
@@ -18,7 +18,7 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
     static final int MIDWEST_BRACKET = 5;
     static final int SOUTH_BRACKET = 6;
     public static final long serialVersionUID = 5609181678399742983L;
-    private boolean isSim=false;//Value that determines whether or not this is a simulated bracket
+
     //Constructor
     /**
      *Cosntructor using an ArrayList of strings to start
@@ -31,16 +31,6 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
         }
     }
 
-    //method: Sets the isSim value to the specified value
-    public void setSim(boolean b)
-    {
-    	isSim=b;
-    }
-    //method: Returns the value of isSim
-    public boolean getSim()
-    {
-    	return isSim;
-    }
     /**
      * Constructor using another Bracket to start
      * @param starting, master bracket pre-simulation
@@ -100,21 +90,6 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
      * @param root, everything below and including this is reset
      */
     public void resetSubtree(int root){
-    	//System.out.println("Root:"+root);
-    	if(root==3)
-    	{
-    		resetFullTree(3);
-    	}
-    	else
-    	if(root==7||root==0)//special case for when clearing the full bracket
-    	{
-    		resetFullTree(3);//resets top left bracket
-    		resetFullTree(4);//resets bottom left bracket
-    		resetFullTree(5);//resets top right bracket
-    		resetFullTree(6);//resets bottom right bracket
-    		resetFullTree(0);//resets the final 4  choices
-    	}
-    	else
         if (root ==0){//special behavior to reset final 4
             for (int i = 0; i < 7; i++) {
                 bracket.set(i,"");
@@ -130,29 +105,6 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
             if (child2 < 64) {
                 resetSubtree(child2);
             }
-            if(root<63)
-            bracket.set(root, "");
-        }
-    }
-    public void resetFullTree(int root)
-    {
-    	//System.out.println("Num: "+root+" TEAM: "+bracket.get(root));
-    	if (root ==0){//special behavior to reset final 4
-            for (int i = 0; i < 7; i++) {
-                bracket.set(i,"");
-            }
-        }
-        else {
-            int child1 = 2 * root + 1;
-            int child2 = 2 * root + 2;
-
-            if (child1 < 64) {//child is above round 1
-                resetFullTree(child1);
-            }
-            if (child2 < 64) {
-                resetFullTree(child2);
-            }
-            if(root<63)
             bracket.set(root, "");
         }
     }
@@ -167,11 +119,25 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
             bracket.set(child,"");
         else {
             int parent = (int) ((child - 1) / 2);
-            if (bracket.get(parent).equals(bracket.get(child))) {
+            
+           if (bracket.get(parent).equals(bracket.get(child))) {
                 removeAbove(parent);
             }
             bracket.set(child, "");
         }
+    }
+    
+    public void removeAboveCurrent(int child, String name) {	//added by zion 4/3 method to compare parent name to child name, this
+    	 if (child==0)													//allows the program to delete the nodes if parent name  = child name
+             bracket.set(child,"");										//AND if the current position has already been set to ""
+         else {														
+             int parent = (int) ((child - 1) / 2);
+             
+            if (bracket.get(parent).equals(name)) {
+                 removeAboveCurrent(parent,name);
+             }
+             bracket.set(child, "");
+         }
     }
 
     /**
@@ -307,4 +273,3 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
         return teamScores[index];
     }
 }
-
