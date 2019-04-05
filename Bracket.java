317855e06c0918a14de844435435468368d85f4b
Elizabeth Chapolini <44447670+chapolinieli@users.noreply.github.com>
2019-04-05 18:01:09 -0400

    fixed (hopefully) errors with bracket code
    added things to bracket code from the main branch so hopefully we can merge

--- Bracket.java
+++ Bracket.java
@@ -1,5 +1,5 @@
-
 package marchmadness;
+
 import java.util.ArrayList;
 import java.io.Serializable; 
 
@@ -20,15 +20,16 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
     static final int MIDWEST_BRACKET = 5;
     static final int SOUTH_BRACKET = 6;
     public static final long serialVersionUID = 5609181678399742983L;
+    private boolean isSim=false;//Value that determines whether or not this is a simulated bracket
     
     //Default constructor added by Elizabeth 4/1/19
     public Bracket(){
         bracket = new ArrayList<String>();
         playerName = "default";
         password = "1234";
-        
-    }
 
+    }
+    
     //Constructor
     /**
      *Cosntructor using an ArrayList of strings to start
@@ -41,6 +42,16 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
         }
     }
 
+    //method: Sets the isSim value to the specified value
+    public void setSim(boolean b)
+    {
+    	isSim=b;
+    }
+    //method: Returns the value of isSim
+    public boolean getSim()
+    {
+    	return isSim;
+    }
     /**
      * Constructor using another Bracket to start
      * @param starting, master bracket pre-simulation
@@ -100,6 +111,21 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
      * @param root, everything below and including this is reset
      */
     public void resetSubtree(int root){
+    	//System.out.println("Root:"+root);
+    	if(root==3)
+    	{
+    		resetFullTree(3);
+    	}
+    	else
+    	if(root==7||root==0)//special case for when clearing the full bracket
+    	{
+    		resetFullTree(3);//resets top left bracket
+    		resetFullTree(4);//resets bottom left bracket
+    		resetFullTree(5);//resets top right bracket
+    		resetFullTree(6);//resets bottom right bracket
+    		resetFullTree(0);//resets the final 4  choices
+    	}
+    	else
         if (root ==0){//special behavior to reset final 4
             for (int i = 0; i < 7; i++) {
                 bracket.set(i,"");
@@ -115,6 +141,29 @@ public class Bracket implements Serializable //Hillary: This bracket class is to
             if (child2 < 64) {
                 resetSubtree(child2);
             }
+            if(root<63)
+            bracket.set(root, "");
+        }
+    }
+    public void resetFullTree(int root)
+    {
+    	//System.out.println("Num: "+root+" TEAM: "+bracket.get(root));
+    	if (root ==0){//special behavior to reset final 4
+            for (int i = 0; i < 7; i++) {
+                bracket.set(i,"");
+            }
+        }
+        else {
+            int child1 = 2 * root + 1;
+            int child2 = 2 * root + 2;
+
+            if (child1 < 64) {//child is above round 1
+                resetFullTree(child1);
+            }
+            if (child2 < 64) {
+                resetFullTree(child2);
+            }
+            if(root<63)
             bracket.set(root, "");
         }
     }
